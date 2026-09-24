13b04d5e29140e575ea3c881bcddea6a6e2284eb
rita-bhatta <ritabhatt027@gmail.com>
2026-09-24 18:29:46 +0545

    practice input and variables

--- day1.py
+++ day1.py
@@ -1,2 +1,6 @@
-print("Hello, GitHub!")
-print("This is my first day of practical Python learning.")
+#DAY1 Python practice
+name = input("enter your name : ")
+age = int(input("enter your age : "))
+print("your name is : ",name)
+print("Your age is : ",age)
+print("Next year you will be : ", age+1)
